535898a42524512222e97f1ab031358681ba21ab
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-03-23 02:11:01 -0300

    Add files via upload

---
+++ TabuadaComForDentroDeFor/src/tabuadacomfordentrodofor/TabuadaComForDentroDoFor.java
@@ -0,0 +1,10 @@
+package tabuadacomfordentrodofor;
+public class TabuadaComForDentroDoFor {
+    public static void main(String[] args) {
+
+        int n = 1;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("\nTabuada do: " + i);
+            for (int j = 0; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+}}}}
